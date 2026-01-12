3b11db86056db25dad58888ae71183d45190b7b2
Akito Miura <phenomer@g.hachune.net>
2026-01-12 18:16:44 +0900

    seaweedfs/consul-registrar.shがsincro-s3のヘルスチェックに失敗していた

--- Docker/seaweedfs/consul-registrar.sh
+++ Docker/seaweedfs/consul-registrar.sh
@@ -34,7 +34,7 @@ sed -i \
 echo "[registrar] start loop: register ${SERVICE_NAME} (${SERVICE_ADDRESS}:${SERVICE_PORT})"
 while true; do
 	# sicro-s3が死んでる時は登録しない（または明示deregisterする）
-	if curl -fsS "http://${SERVICE_ADDRESS}:${SERVICE_PORT}/" >/dev/null 2>&1; then
+	if curl -fsS "http://${SERVICE_ADDRESS}:${SERVICE_PORT}/status" >/dev/null 2>&1; then
 		curl -fsS -X PUT "${CONSUL_HTTP_ADDR}/v1/agent/service/register" \
 			--data-binary @/tmp/register.json >/dev/null
 		echo "[registrar] registered/renewed"
