11965ef612e817824cb4a613507182a6cd8f94ff
Akito Miura <phenomer@g.hachune.net>
2026-02-08 21:12:36 +0900

    曖昧だったtalk_modeをenumに変更

--- sincromisor-server/sincro-rtc/src/sincro_rtc/models/RTCSessionOffer.py
+++ sincromisor-server/sincro-rtc/src/sincro_rtc/models/RTCSessionOffer.py
@@ -1,7 +1,16 @@
+from enum import Enum
+
 from pydantic import BaseModel
 
+
+class TalkMode(str, Enum):
+    # chat: 対話
+    # sincro: 音声認識 + 読み上げ
+
+    chat = "chat"
+    sincro = "sincro"
 
 class RTCSessionOffer(BaseModel):
     sdp: str
     type: str
-    talk_mode: str
+    talk_mode: TalkMode
